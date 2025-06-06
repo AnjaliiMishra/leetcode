dbcc5b6224229033b1b647bfd3e813ee60642be9
Anjali Mishra <anj121704@gmail.com>
2025-06-06 23:16:49 +0530

    Time: 2 ms (41.96%) | Memory: 42.1 MB (90.51%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,19 +1,93 @@
 class Solution {
-  public String addBinary(String a, String b) {
-    StringBuilder sb = new StringBuilder();
-    int carry = 0;
-    int i = a.length() - 1;
-    int j = b.length() - 1;
+    public String addBinary(String a, String b) {
+        
+        int i = a.length() - 1;
+        int j = b.length() - 1;
 
-    while (i >= 0 || j >= 0 || carry == 1) {
-      if (i >= 0)
-        carry += a.charAt(i--) - '0';
-      if (j >= 0)
-        carry += b.charAt(j--) - '0';
-      sb.append(carry % 2);
-      carry /= 2;
+        StringBuilder sb = new StringBuilder();
+        char carry = '0';
+
+        while (i >= 0 && j >= 0) {
+
+            if (a.charAt(i) == '1' && b.charAt(j) == '0') {      
+                if (carry == '1') {
+                    sb.insert(0, '0');
+                }
+                else {
+                    sb.insert(0, '1');
+                }
+            }
+            else if (a.charAt(i) == '0' && b.charAt(j) == '1') {
+                if (carry == '1') {
+                    sb.insert(0, '0');
+                }
+                else {
+                    sb.insert(0, '1');
+                }
+            }
+            else if (a.charAt(i) == '0' && b.charAt(j) == '0') {
+
+                if (carry == '1')
+                    sb.insert(0, '1');
+                else
+                    sb.insert(0, '0');
+
+                    carry = '0';
+            }
+            else {
+                if (carry == '1') {
+                    sb.insert(0, '1');
+                }
+                else {
+                    sb.insert(0, '0');
+                }
+                carry = '1';
+            }
+            i--;
+            j--;
+        }
+
+        while (i >= 0) {
+
+            if (a.charAt(i) == '1') {
+                if (carry == '1')
+                    sb.insert(0, '0');
+                else 
+                    sb.insert(0, '1');
+            }
+            else {
+                if (carry == '1') {
+                    sb.insert(0, '1');
+                    carry = '0';
+                }
+                else {
+                    sb.insert(0, '0');
+                }
+            }
+            i--;
+        }
+        while (j >= 0) {
+
+            if (b.charAt(j) == '1') {
+                if (carry == '1')
+                    sb.insert(0, '0');
+                else 
+                    sb.insert(0, '1');
+            }
+            else {
+                if (carry == '1') {
+                    sb.insert(0, '1');
+                    carry = '0';
+                }
+                else {
+                    sb.insert(0, '0');
+                }
+            }
+            j--;
+        }
+        if (carry == '1')
+            sb.insert(0, '1');
+
+        return sb.toString();
     }
-
-    return sb.reverse().toString();
-  }
 }
