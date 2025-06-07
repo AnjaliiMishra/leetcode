9d6b8a669daec3f676ba8acaac15938798c3ed75
Anjali Mishra <anj121704@gmail.com>
2025-06-08 00:17:17 +0530

    Time: 3 ms (1.75%) | Memory: 42 MB (67.23%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,11 +1,19 @@
 class Solution {
-  public int removeElement(int[] nums, int val) {
-    int i = 0;
+    public int removeElement(int[] nums, int val) {
+        if(nums.length ==0 ) return 0;
 
-    for (final int num : nums)
-      if (num != val)
-        nums[i++] = num;
+        int min = nums[0];
+        int len = nums.length;
+        for(int i=0; i<= nums.length-1;i++){
 
-    return i;
-  }
+            if(nums[i]==val) {
+                nums[i] = Integer.MAX_VALUE;
+                len--;
+            }
+        }
+        Arrays.sort(nums);
+        System.out.print(len);
+        return len;
+        
+    }
 }
